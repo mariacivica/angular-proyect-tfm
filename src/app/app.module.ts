b6ee013d8773074877c470d1271cdb396732223a
María González Bejarano <maria.gonzalez@civica-soft.com>
2024-04-11 15:02:20 +0200

    Components - Header & Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';    //los modulos antes que la definicion de componentes
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule, //importacion del modulo shared
   ],
   providers: [],
   bootstrap: [AppComponent]
