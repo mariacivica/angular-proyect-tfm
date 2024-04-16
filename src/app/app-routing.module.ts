e34c68a6980e687c87350593a5c0682808e474a5
María González Bejarano <maria.gonzalez@civica-soft.com>
2024-04-16 14:22:40 +0200

    Components & Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './auth/login-form/login-form.component';
+import { HomeComponent } from './auth/home/home.component';
+import { RegisterFormComponent } from './auth/register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio-sesion', component: LoginFormComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'registro', component: RegisterFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { SharedModule } from './shared/shared.module';    //los modulos antes qu
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './auth/login-form/login-form.component';
+import { RegisterFormComponent } from './auth/register-form/register-form.component';
+import { HomeComponent } from './auth/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
